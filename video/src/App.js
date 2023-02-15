ca888e4519629c59aab2d91e05432e75c2fa0809
galya-cmd <galya1985@bk.ru>
2023-02-15 13:33:41 +0300

    исправила ошибки

--- video/src/App.js
+++ video/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import MyComponent from './fech';
 import './App.css';
-import ROUTES from '../../routes/routes';
-import { PageWrapper } from './layout/page-wrapper';
-import { ActorsPage } from './pages/actors-page';
-import { HomePage } from '../pages/home-page';
-import { SearchPage } from '../pages/keyword-search-page';
-import { MoviePage } from '../pages/movie-page';
-import { PersonalityPage } from '../pages/personality-page';
-import { PopularPage } from '../pages/popular-page';
-import { Top250Page } from '../pages/top250-page';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <PageWrapper />
-      <ActorsPage/>
+  
+    return(
+      <div className='App'>
+      <MyComponent/>
+      </div>
+    )
 
-    </div>
-  );
+
+
+  
+
 }
-
 export default App;
